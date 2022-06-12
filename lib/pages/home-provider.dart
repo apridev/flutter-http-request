69527bd289e9d4e3251d97a7fa0d15d1bb25ca75
Apridev <apriantofajar11@gmail.com>
2022-06-12 13:37:15 +0900

    home provider

---
+++ lib/pages/home-provider.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:http_request/models/http_post_provider.dart';
+import 'package:provider/provider.dart';
+
+class HomeProvider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("Rebuild");
+    final httpGetProvider = Provider.of<HttpPostProvider>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("POST - PROVIDER"),
+      ),
+      body: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer<HttpPostProvider>(
+              builder: (context, value, child) => FittedBox(
+                child: Text( (value.data["id"] == null)
+                  ? "ID : Belum ada data"
+                  : "ID : ${value.data["id"]}",
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            FittedBox(child: Text("Name : ", style: TextStyle(fontSize: 14))),
+            Consumer<HttpPostProvider>(
+              builder: (context, value, child) => FittedBox(
+                child: Text((value.data["name"] == null)
+                 ? "Belum ada data"
+                 : "${value.data["name"]}",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            FittedBox(child: Text("Job : ", style: TextStyle(fontSize: 14))),
+            Consumer<HttpPostProvider>(
+              builder: (context, value, child) => FittedBox(
+                child: Text((value.data["job"] == null)
+                  ? "Belum ada data"
+                  : "${value.data["job"]}",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            FittedBox(
+                child: Text("Created At : ", style: TextStyle(fontSize: 14))),
+            Consumer<HttpPostProvider>(
+              builder: (context, value, child) => FittedBox(
+                child: Text((value.data["createdAt"] == null)
+                  ? "Belum ada data"
+                  : "${value.data["createdAt"]}",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 100),
+            OutlinedButton(
+              onPressed: () {
+                httpGetProvider.connectAPI("Pablo Amore", "Back End Developer");
+              },
+              child: Text(
+                "POST DATA",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
